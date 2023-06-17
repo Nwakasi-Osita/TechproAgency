3cf3f5b85ab264ba51c2f4eb5898f7f3a3fdf4a8
Nwakasi Osita <nwakasiosita081@gmail.com>
2023-06-17 18:39:41 +0100

    implemented hero, services and how we work sections

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import styles from "./Nav.module.scss";
+
+
+export default function Nav() {
+  return (
+    <>
+    <div className={styles.techproHeaderSection}>
+      <div className={styles.techproHeaderLink}>
+        <div className={styles.techproTitle}><h4>TechPro Agency</h4>
+        </div>
+        <div className={styles.techproStartUpButton}><p>Let&apos;s work together</p></div>
+      </div>
+
+
+    </div>
+    </>
+  );
+}
